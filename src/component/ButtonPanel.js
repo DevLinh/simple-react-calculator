df7527cdaecf10c7248062da7865d2f529e9899d
DevLinh <DevLinh99QB@gmail.com>
2019-11-06 09:14:35 +0700

    create blank components

---
+++ src/component/ButtonPanel.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ButtonPanel extends Component {
+  render() {
+    return <div>Hello ButtonPanel</div>;
+  }
+}
+
+export default ButtonPanel;
---
+++ src/component/Display.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Display extends Component {
+  render() {
+    return <div>Hi Display</div>;
+  }
+}
+
+export default Display;
